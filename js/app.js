d8280b1aeed85d58f8acfc75c7251f8f70819a3b
ABiviano13 <abiviano0@outlook.com>
2023-02-14 16:12:14 +0100

    COMPLETED stamp console info

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-// DATI
-// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
-// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+///FUNZIONI///
+function getFullName() {
+	// console.log(this)
+	const firstName = this['firstName']
+	const lastName = this.lastName
+	return `${firstName} ${lastName}`
+}
 
 //Creiamo un array di oggetti e forniamo le seguenti informazioni (nome, ruolo, img)
 const persona = {
     firstName: 'Wayne',
     lastName: 'Barnett',
     role: 'Founder & CEO',
-    img: 'wayne-barnett-founder-ceo.jpg'
+    img: 'wayne-barnett-founder-ceo.jpg',
+    getFullName: getFullName
 };
 
 const teamArray = [
@@ -20,41 +21,56 @@ const teamArray = [
         firstName: 'Angela',
         lastName: 'Caroll',
         role: 'Chief Editor',
-        img: 'angela-caroll-chief-editor.jpg'
+        img: 'angela-caroll-chief-editor.jpg',
+        getFullName: getFullName
     },
     {
         firstName: 'Walter',
         lastName: 'Gordon',
         role: 'Office Manager',
-        img: 'walter-gordon-office-manager.jpg'
+        img: 'walter-gordon-office-manager.jpg',
+        getFullName: getFullName
     },
     {
         firstName: 'Angela',
         lastName: 'Lopez',
         role: 'Social Media Manager',
-        img: 'angela-lopez-social-media-manager.jpg'
+        img: 'angela-lopez-social-media-manager.jpg',
+        getFullName: getFullName
     },
     {
         firstName: 'Scott',
         lastName: 'Estrada',
         role: 'Developer',
-        img: 'scott-estrada-developer.jpg'
+        img: 'scott-estrada-developer.jpg',
+        getFullName: getFullName
     },
     {
         firstName: 'Barbara',
         lastName: 'Ramos',
         role: 'Graphic Designer',
-        img: 'barbara-ramos-graphic-designer.jpg'
+        img: 'barbara-ramos-graphic-designer.jpg',
+        getFullName: getFullName
     }
 
 ];
 
-console.log(teamArray);
+// console.log(teamArray);
 
 //Stampare in console le informazioni di ogni membro del team (for-in)
 for(let teamInfo in teamArray){
     // console.log(teamArray[teamInfo]);
 
     const teamArrayValue = teamArray[teamInfo];
-    console.log(teamArrayValue);
+    // console.log(teamArrayValue);
+}
+
+for(let i = 0; i < teamArray.length; i++){
+    const personaCorrente = teamArray[i];
+
+    const fullName = personaCorrente.getFullName()
+    const role = personaCorrente.role;
+    const imgPersona = personaCorrente.img;
+
+    console.log(fullName, role, imgPersona);
 }
